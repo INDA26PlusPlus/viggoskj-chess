68fb3bac714f283514477d02448cb19d48c56e23
ViggoTrustAnchor <viggo@trustanchorgroup.com>
2026-09-18 08:14:12 +0200

    tests

--- viggoskj-chess-lib/src/tests/check_mate_tests.rs
+++ viggoskj-chess-lib/src/tests/check_mate_tests.rs
@@ -1,7 +1,9 @@
 #[cfg(test)]
 mod tests {
     use crate::{
-         check::{CheckState, get_check_state}, game::{Color}, tests::tests::{game_from_board_advanced},
+        check::{CheckState, get_check_state},
+        game::Color,
+        tests::tests::game_from_board_advanced,
     };
 
     #[test]
@@ -20,5 +22,57 @@ mod tests {
         );
 
         assert_eq!(get_check_state(&game), Some(CheckState::Checkmate));
+    }
+
+    #[test]
+    fn stalemate_1() {
+        let game = game_from_board_advanced(
+            "
+--------
+--------
+--------
+--r----r
+---K----
+----r--r
+--------    
+--------",
+            Color::White,
+        );
+    }
+
+    #[test]
+    fn check_1() {
+        let game = game_from_board_advanced(
+            "
+--------
+--------
+--------
+--------
+---K---r
+--------
+--------    
+--------",
+            Color::White,
+        );
+
+        assert_eq!(get_check_state(&game), Some(CheckState::Check));
+    }
+
+    #[test]
+    fn none_1() {
+        let game = game_from_board_advanced(
+            "
+--------
+--------
+--------
+--------
+---K----
+----n---
+--------    
+--------",
+            Color::White,
+        );
+
+        assert_eq!(get_check_state(&game), None);
     }
 }
